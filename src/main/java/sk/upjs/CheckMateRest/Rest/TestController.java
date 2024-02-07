6932e7b3ffeca2a3a3c70eb3d1dfe17a728d3955
andrek13 <andreshko1305@gmail.com>
2024-02-07 17:12:39 +0100

    updated controller

---
+++ src/main/java/sk/upjs/CheckMateRest/Rest/TestController.java
@@ -0,0 +1,63 @@
+package sk.upjs.CheckMateRest.Rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import sk.upjs.CheckMateRest.storage.EntityNotFoundException;
+import sk.upjs.CheckMateRest.storage.dao.TestDao;
+import sk.upjs.CheckMateRest.storage.triedy.Test;
+
+import java.sql.Time;
+import java.util.Date;
+import java.util.List;
+
+public class TestController {
+
+    private final TestDao testDao;
+
+    public TestController(TestDao testDao) {
+        this.testDao = testDao;
+    }
+
+    @PostMapping("/tests")
+    public void save(Test test) throws EntityNotFoundException {
+        testDao.save(test);
+    }
+
+    @GetMapping("/tests/{user_id}")
+    public List<Test> getAllUserTests(@PathVariable int user_id) throws EntityNotFoundException {
+        List<Test> allUserTests = testDao.getAllUserTests(user_id);
+        return allUserTests;
+    }
+
+    @GetMapping("/tests/testsOnTime/{time}/{date}")
+    public int testsOnTime(@PathVariable Time time, @PathVariable Date date){
+        return testDao.testsOnTime(time, date);
+    }
+
+    @GetMapping("/tests/getTestById/{testId}")
+    public Test getTestById(@PathVariable long testId){
+        return  testDao.getTestById(testId);
+    }
+
+    @PostMapping("/tests/updateTestResultById/{testId}/{newResult}")
+    public void updateTestResultById(@PathVariable int testId, @PathVariable int newResult) throws EntityNotFoundException {
+        testDao.updateTestResultById(testId, newResult);
+    }
+
+    @GetMapping("/tests/getUsersPCRTest/{userId}")
+    public Test getUsersPCRTest(@PathVariable int userId){
+        return testDao.getUsersPCRTest(userId);
+    }
+
+    @GetMapping("/tests/getUsersNAATsTest/{userId}")
+    public Test getUsersNAATsTest(@PathVariable int userId){
+        return testDao.getUsersNAATsTest(userId);
+    }
+
+    @GetMapping("/tests/getTestsOlderThanTenMinutes/{userId}")
+    public List<Test> getTestsOlderThanTenMinutes(@PathVariable int userId){
+        return testDao.getTestsOlderThanTenMinutes(userId);
+    }
+
+}
